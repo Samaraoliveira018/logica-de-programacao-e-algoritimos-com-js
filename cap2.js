15b3ac08004f85d6d3b7aa5fd07d272b2b02dc95
Samaraoliveira018 <78242355+Samaraoliveira018@users.noreply.github.com>
2023-10-22 18:50:35 -0400

    Update cap2.js

--- cap2.js
+++ cap2.js
@@ -1,3 +1,4 @@
+function olaVoce(){
 const form1 = document.querySelector('form');
 const result = document.querySelector('h3');
 form1.addEventListener('submit',(e) => {
@@ -5,7 +6,10 @@ form1.addEventListener('submit',(e) => {
     result.innerText = `Olá ${nome}`
     e.preventDefault();
 })
+}
 
+
+function converterHorasEminutos(){
 const form2 = document.querySelector('form');
 const resp1 = document.querySelector('h3');
 const resp2 = document.querySelector('h4');
@@ -21,8 +25,11 @@ form2.addEventListener('submit',(e) => {
     resp2.innerText = `${horas} hora(s) e ${minutos} minuto(s)`
     console.log(resp1, resp2)
     e.preventDefault();
-})
+})    
+}
 
+
+function promoVeiculo(){
 const formVeiculo = document.querySelector('form');
 const promoção = document.querySelector('#nomeDoCarro');
 const entrada = document.querySelector('#entrada');
@@ -41,3 +48,40 @@ formVeiculo.addEventListener('submit',(e) => {
 
     e.preventDefault();
 })
+}
+
+
+function totalApagarRestaurante(){
+const formBuffet = document.querySelector('form');
+const valorTotal = document.querySelector('#valorTotal');
+
+formBuffet.addEventListener('submit',(e) => {
+    const valorBuffetKL = formBuffet.buffetkl.value
+    const consumo = formBuffet.consumo.value
+    const valorPagar = (valorBuffetKL / 1000) * consumo
+    
+    valorTotal.innerText = `Valor a pagar R$: ${valorPagar.toFixed(2)}`
+    
+    e.preventDefault();
+})
+}
+
+
+// ******************farmacia
+
+function promoFarmacia(){
+const formFarm = document.querySelector('form');
+const titleRemed = document.querySelector('#titleRemed');
+const valor = document.querySelector('#valor');
+
+formFarm.addEventListener('submit',(e) => {
+    const nomeMedicamento = formFarm.nomeMedic.value
+    const precoMedicamento = formFarm.preco.value
+    const medicamento = Math.floor(precoMedicamento * 2)
+    
+    titleRemed.innerText = `Promoção de: ${nomeMedicamento}`
+    valor.innerText = `Valor a pagar R$: ${medicamento.toFixed(2)}`
+    
+    e.preventDefault();
+})
+}
